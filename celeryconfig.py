e8b888855bc0a536a365e1550a9622dd7788a849
KumailQazi <tag.kumail@gmail.com>
2019-08-14 23:39:47 +0500

    Setting up celery environment & configuration.

---
+++ celeryconfig.py
@@ -0,0 +1,9 @@
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+timezone = 'Europe/Oslo'
+enable_utc = True
+
+task_annotations = {
+    'tasks.add': {'rate_limit': '10/m'}
+}
---
+++ tasks.py
@@ -0,0 +1,8 @@
+from celery import Celery
+
+app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//')
+app.config_from_object('celeryconfig.py')
+
+@app.task
+def add(x, y):
+    return x + y
