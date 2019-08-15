3e035516b6aefca695da44f91b1df7f9ec81d4cf
KumailQazi <tag.kumail@gmail.com>
2019-08-15 11:41:44 +0500

    Commenting

--- tasks.py
+++ tasks.py
@@ -2,11 +2,11 @@ from celery import Celery
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//')
 
-app.config_from_object('celeryconfig')
+app.config_from_object('celeryconfig.py')
 
 @app.task
 def add(x, y):
     return x + y
-
+# Calling task and storing result
 result = add.delay(4, 4)
 print(result)
