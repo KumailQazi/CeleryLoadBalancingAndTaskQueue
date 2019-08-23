ed5b44e029a95792af05a94d0c0c9469ca0d9de0
KumailQazi <tag.kumail@gmail.com>
2019-08-23 10:33:33 +0500

    Updating Timezone

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,9 +2,11 @@
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
-timezone = 'Europe/Oslo'
+timezone='Asia/Karachi'
 enable_utc = True
 
 task_annotations = {
     'tasks.add': {'rate_limit': '10/m'}
 }
+
+# timezone='Asia/Karachi'
