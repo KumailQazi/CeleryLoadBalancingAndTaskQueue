26f8462005201d9bad3fd8094114609a31139ba3
KumailQazi <tag.kumail@gmail.com>
2019-08-26 10:50:58 +0500

    Task routes added. Broker changed to amqp

--- proj/celery.py
+++ proj/celery.py
@@ -4,13 +4,17 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
 # Creating a celery instance
-app = Celery('proj', broker='amqp://', backend='rpc://', include=['proj.tasks'])
+app = Celery('proj', broker='amqp://', backend='amqp://', include=['proj.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
     result_expires=3600,
 )
-
+app.conf.update(
+    task_routes = {
+        'proj.tasks.add': {'queue': 'hipri'},
+    },
+)
 
 # Starting the App
 if __name__ == '__main__':
